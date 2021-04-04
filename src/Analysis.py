9b59ca67dfb67fd88f7b9b8dffddba87723c1ef4
Manuel <leichti@gmail.com>
2021-04-04 22:09:14 +0200

    just testing around

--- src/Analysis.py
+++ src/Analysis.py
@@ -2,3 +2,11 @@
 class ElementalAnalyses:
     def __init__(self, file):
         pass
+
+class RemFile:
+    def __init__(self, path):
+        pass
+
+class FileRow(RemFile):
+    def __init__(self):
+        pass
